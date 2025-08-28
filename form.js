2b70f397a902f24e9af8eaca9caeb82d1fb70b25
Anto-peppin <antopeppin22@gmail.com>
2025-08-28 20:48:16 +0530

    check the mediaquery

---
+++ form.js
@@ -0,0 +1,9 @@
+let form = document.querySelector('form')
+let input = document.querySelectorAll('input')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    input.forEach()
+    validation()
+
+})
